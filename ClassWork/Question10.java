4c436d6ecf8413121fcc745207b7c78b3736ee31
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-19 10:51:59 +0530

    Question 10 updated

--- ClassWork/Question10.java
+++ ClassWork/Question10.java
@@ -1,20 +1,35 @@
 package Bridgelabz_Training.ClassWork;
 
 public class Question10 {
-  static {
-    System.out.println("Inside Static Variable");
-  }
-  {
-    int val = 20;
-    System.out.println("Inside Instance");
-  }
+  // static {
+  // System.out.println("Inside Static Variable");
+  // }
+  // {
+  // int val = 20;
+  // System.out.println("Inside Instance");
+  // }
+
+  // public void display() {
+  // System.out.println("Inside Local Variable");
+  // }
+
+  // public static void main(String[] args) {
+  // Question10 res = new Question10();
+  // res.display();
+  // }
+  int num1 = 10;
+  static int num2 = 20;
 
   public void display() {
-    System.out.println("Inside Local Variable");
+    int num3 = 30;
+    System.out.println("Instance Value -> " + num1);
+    System.out.println("Static Value -> " + num2);
+    System.out.println("Local Value -> " + num3);
   }
 
   public static void main(String[] args) {
     Question10 res = new Question10();
     res.display();
   }
+
 }
