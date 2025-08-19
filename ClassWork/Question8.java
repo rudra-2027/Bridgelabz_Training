bcee3a50be56ad5723f8a17a09c04220d301f5be
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-19 10:24:22 +0530

    Question 8 done

---
+++ ClassWork/Question8.java
@@ -0,0 +1,13 @@
+package Bridgelabz_Training.ClassWork;
+
+public class Question8 {
+  static int val;
+  static {
+    val = 20;
+
+  }
+
+  public static void main(String[] args) {
+    System.out.println(Question8.val);
+  }
+}
