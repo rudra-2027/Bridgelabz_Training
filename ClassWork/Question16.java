f2bee91d7bea6a5cc70c34383284c893c48a4fc4
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-19 11:23:52 +0530

    Question 16 done

---
+++ ClassWork/Question16.java
@@ -0,0 +1,14 @@
+package Bridgelabz_Training.ClassWork;
+
+public class Question16 {
+  public static void main(String[] args) {
+    int val;
+    System.out.println(val);
+    /*
+     * Exception in thread "main" java.lang.Error: Unresolved compilation problem:
+     * The local variable val may not have been initialized
+     * 
+     * at sectionF_BridgeLabz_Training.Question16.main(Question16.java:6)
+     */
+  }
+}
