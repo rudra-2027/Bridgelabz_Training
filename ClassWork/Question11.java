45a578cbb9beea2f07f44247c50a2c41bf5135d6
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-19 10:41:29 +0530

    Question 11 done

---
+++ ClassWork/Question11.java
@@ -0,0 +1,12 @@
+package Bridgelabz_Training.ClassWork;
+
+public class Question11 {
+  static int val;
+
+  public static void main(String[] args) {
+    System.out.println("Without assign the default value  " + val);
+    val = 50;
+    System.out.println("After assign  the value " + val);
+
+  }
+}
