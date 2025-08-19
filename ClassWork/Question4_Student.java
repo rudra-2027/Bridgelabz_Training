ef058b4721f4498ef237c9790c7ae1cb5591c443
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-19 09:15:29 +0530

    Question 4 done

--- ClassWork/Question4_Student.java
+++ ClassWork/Question4_Student.java
@@ -3,4 +3,23 @@ package Bridgelabz_Training.ClassWork;
 public class Question4_Student {
   String name;
   float marks;
+
+  public Question4_Student(String name, float marks) {
+    this.name = name;
+    this.marks = marks;
+
+  }
+
+  public void stdDetails() {
+    System.out.println(name + " " + marks);
+  }
+
+  public static void main(String[] args) {
+    Question4_Student std1 = new Question4_Student("Rudra", 90);
+    Question4_Student std2 = new Question4_Student("Utkarsh", 95.5F);
+    std1.stdDetails();
+    std2.stdDetails();
+
+  }
+
 }
