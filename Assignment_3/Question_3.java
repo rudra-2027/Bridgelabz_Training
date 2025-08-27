ccebae7b061b15e42f3a4281428ff977db0633c4
rudra-2027 <144617358+rudra-2027@users.noreply.github.com>
2025-08-27 11:36:38 +0530

    Assignment - 3 Donee

--- Assignment_3/Question_3.java
+++ Assignment_3/Question_3.java
@@ -12,15 +12,15 @@ import java.util.Scanner;
 //
 //Hint: Use DateTimeFormatter with custom patterns for date formatting.
 public class Question_3 {
-	public static void main(String[] args) {
-		LocalDate date = LocalDate.now();
-		DateTimeFormatter format1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        DateTimeFormatter format2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        DateTimeFormatter format3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
-        
-        System.out.println("Format 1 (dd/MM/yyyy): " + date.format(format1));
-        System.out.println("Format 2 (yyyy-MM-dd): " + date.format(format2));
-        System.out.println("Format 3 (EEE, MMM dd, yyyy): " + date.format(format3));
-	}
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        DateTimeFormatter f1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter f2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        DateTimeFormatter f3 = DateTimeFormatter.ofPattern("EEE, MMM dd, yyyy");
+
+        System.out.println("Format 1 (dd/MM/yyyy): " + date.format(f1));
+        System.out.println("Format 2 (yyyy-MM-dd): " + date.format(f2));
+        System.out.println("Format 3 (EEE, MMM dd, yyyy): " + date.format(f3));
+    }
 
 }
--- Assignment_3_Part2/Question_7.java
+++ Assignment_3_Part2/Question_7.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 //○ Use separate functions for GCD and LCM calculations, showcasing how modular
 //code works.
 public class Question_7 {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         int[] numbers = getInput();
         int a = numbers[0];
         int b = numbers[1];
@@ -18,28 +18,30 @@ public class Question_7 {
 
         displayResults(a, b, gcdValue, lcmValue);
     }
-	public static int[] getInput() {
-		Scanner sc = new Scanner(System.in);
+
+    public static int[] getInput() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
-        sc.close();
-        return new int[]{a, b};
-	}
-	public static int gcd(int a, int b) {
+
+        return new int[] { a, b };
+    }
+
+    public static int gcd(int a, int b) {
         if (b == 0) {
-            return a; 
+            return a;
         }
         return gcd(b, a % b);
     }
 
-    
     public static int lcm(int a, int b) {
         return (a * b) / gcd(a, b);
     }
+
     public static void displayResults(int a, int b, int gcd, int lcm) {
-    	System.out.println("GCD of " + a + " and " + b + " = " + gcd);
+        System.out.println("GCD of " + a + " and " + b + " = " + gcd);
         System.out.println("LCM of " + a + " and " + b + " = " + lcm);
     }
 }
